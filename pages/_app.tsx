95c242ecb1005e7d67181400a55ddfbcdbf575ab
Juan Araneta <juanaraneta10@gmail.com>
2023-02-27 11:37:06 -0300

    Add initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { getLibrary } from "../config/web3/";
+import { Web3ReactProvider } from "@web3-react/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
 }
